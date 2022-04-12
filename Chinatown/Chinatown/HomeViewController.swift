49ae744a50baf2cfb5550b684204fb940eb3f8b8
Jeongin Yoon <jy3524@nyu.edu>
2022-04-12 18:09:22 -0400

    working on map

--- Chinatown/Chinatown/HomeViewController.swift
+++ Chinatown/Chinatown/HomeViewController.swift
@@ -3,20 +3,68 @@
 //  Chinatown
 //
 //  Created by Cleo Xiao on 4/10/22.
+//  Updated by Jeongin Yoon on 4/12/22. (MapKit)
 //
 
 import UIKit
+import MapKit
+import CoreLocation
 
-class HomeViewController: UIViewController {
+class HomeViewController: UIViewController, MKMapViewDelegate {
+    
+    let map = MKMapView()
+    
+    let coordinate = CLLocationCoordinate2D(
+        latitude: 40.71521,
+        longitude: -73.99869
+    )
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        view.addSubview(map)
+        map.frame = view.bounds
         
-        title = "Home"
+        map.setRegion(MKCoordinateRegion(
+            center: coordinate,
+            span: MKCoordinateSpan(
+                latitudeDelta: 0.1,
+                longitudeDelta: 0.1)
+        ),
+        animated: false)
+        
+        map.delegate = self
+        
+        addCustomPin()
 
         // Do any additional setup after loading the view.
     }
     
+    private func addCustomPin() {
+        let pin = MKPointAnnotation()
+        pin.coordinate = coordinate
+        pin.title = "Wing On Wo"
+        pin.subtitle = "Try AR here"
+        map.addAnnotation(pin)
+    }
+    
+    func mapView(_ mapView: MKMapView, viewFor annotation: MKAnnotation) -> MKAnnotationView? {
+        guard !(annotation is MKUserLocation) else {
+            return nil
+        }
+        var annotationView = map.dequeueReusableAnnotationView(withIdentifier: "custom")
+        
+        if annotationView == nil {
+            annotationView = MKAnnotationView(annotation: annotation,
+                                              reuseIdentifier: "custom")
+            annotationView?.canShowCallout = true
+        } else {
+            annotationView?.annotation = annotation
+        }
+        
+        annotationView?.image = UIImage(named: "WingOnWoIcon")
+        
+        return annotationView
+    }
 
     /*
     // MARK: - Navigation

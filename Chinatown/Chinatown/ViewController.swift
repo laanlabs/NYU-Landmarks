cc2f5b3791c14290c26bd9a2b67b3984882c6093
cleoxiao <cleoxiao@outlook.com>
2022-04-28 21:22:46 -0400

    Add AR filters for WOW

--- Chinatown/Chinatown/ViewController.swift
+++ Chinatown/Chinatown/ViewController.swift
@@ -16,7 +16,7 @@ class ViewController: UIViewController {
         super.viewDidLoad()
         
         // Load the "Box" scene from the "Experience" Reality File
-        let boxAnchor = try! Test.loadScene()
+        let boxAnchor = try! Experience.loadBox()
         
         // Add the box anchor to the scene
         arView.scene.anchors.append(boxAnchor)
